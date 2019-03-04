fd3a518fd00cc1ce11f623f7c76ba9ec04be5222
aitexi <ukat.awi@gmail.com>
2019-03-05 01:38:31 +0900

    タイトル画面＆ポーズ画面

---
+++ Assets/Iwataku/Scripts/Pause.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject pauseUIPrefab;
+	private GameObject pauseUIInstance;
+	void Update () {
+		if(Input.GetKeyDown("q")){
+			Debug.Log("put q!");
+			if(pauseUIInstance == null){
+				pauseUIInstance = GameObject.Instantiate(pauseUIPrefab) as GameObject;
+				Time.timeScale = 0f;
+			}else{
+				Destroy(pauseUIInstance);
+				Time.timeScale = 1f;
+			}
+		}
+	}
+}
