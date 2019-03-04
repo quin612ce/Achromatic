ef3d75bb9503bb2e9b2577f5c96b0b2c77ca95ad
quin612ce <meyuharin1101@gmail.com>
2019-03-04 22:25:43 +0900

    菊池くんつおい

--- Assets/Scenes/Scripts/Zenhoui.cs
+++ Assets/Scenes/Scripts/Zenhoui.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Zenhoui : MonoBehaviour {
-	/*public GameObject bullet;
+	public GameObject bullet;
 
 	//Use this for initialization
 	void Start () {
-		
+		StartCoroutine (Update_coroutine());
 	}
 
-	IEnumerator Update()
+	IEnumerator Update_coroutine()
 	{
 		int count = 0;
 		while (true)
 		{
-			for (int rad = 0; rad < 360; rad += 6)
+			for (int rad = 0; rad < 360; rad += 8)
 			{
-				Instantiate (bullet, transform.position, Quaternion.identity);
+				Instantiate (bullet, this.transform.position, Quaternion.Euler (this.transform.rotation.eulerAngles+ new Vector3(0, 0, rad)));
 			}
-			yield return new WaitForSeconds (8.0f);
+			yield return new WaitForSeconds (3.0f);
 			count++;
+			Debug.Log ("unchi");
 		}
-	}*/
+	}
 }
