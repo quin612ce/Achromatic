d7a58b0716df3e5a2cef7609e563b438add39588
quin612ce <meyuharin1101@gmail.com>
2019-03-04 23:59:54 +0900

    Uzumaki.cs追加

---
+++ Assets/script_danmaku/Uzumaki.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uzumaki : MonoBehaviour {
+	public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Update_coroutine());
+	}
+	
+	// Update is called once per frame
+	IEnumerator Update_coroutine() 
+	{
+		int rad = 0;
+		{
+			while(true)
+			{
+				Instantiate (bullet, this.transform.position, Quaternion.Euler (this.transform.rotation.eulerAngles+ new Vector3(0, 0, rad)));
+				rad += 8;
+            yield return new WaitForSeconds(0.05f);
+			}
+		}
+	}
+}
