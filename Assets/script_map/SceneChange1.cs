5fa1ff642a3b5366249e3bfcd8e3a271041954b9
leo_rabbit30 <mio.rabbit971.snow@gmail.com>
2019-03-04 14:55:18 +0900

    シーン遷移、メッセージの表示、入れないエリアを追加しました

---
+++ Assets/script_map/SceneChange1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange1 : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D oth)
+    {
+        if (oth.transform.tag == "Player")
+        {
+            SceneManager.LoadScene("PlayMap2");
+        }
+    }
+}
---
+++ Assets/script_map/message.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class message : MonoBehaviour {
+	
+	public GameObject Message;
+
+	void OnCollisionEnter2D(Collision2D oth)
+    {
+        if (oth.transform.tag == "Player")
+        {
+            Message.SetActive (true);;
+        }
+    }
+}
