4bdf7f8ae785709cab2e849c38ebbf112f210e6f
leo_rabbit30 <mio.rabbit971.snow@gmail.com>
2019-03-04 13:14:05 +0900

    add map scenes

---
+++ Assets/script_map/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.Translate (0.1f, 0, 0);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+			this.gameObject.transform.Translate (-0.1f, 0, 0);
+        }
+		else if(Input.GetKey(KeyCode.UpArrow))
+        {
+			this.gameObject.transform.Translate (0, 0.1f, 0);
+        }
+		else if(Input.GetKey(KeyCode.DownArrow))
+        {
+			this.gameObject.transform.Translate (0, -0.1f, 0);
+        }
+    }
+}
