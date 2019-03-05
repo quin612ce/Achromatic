3bf68b533d26d324ebcde11c7e9101628584938c
aitexi <ukat.awi@gmail.com>
2019-03-05 14:58:11 +0900

    セーブ＆ロード

--- Assets/Iwataku/Scripts/BackTitleButton.cs
+++ Assets/Iwataku/Scripts/BackTitleButton.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadButton : MonoBehaviour {
+public class BackTitleButton : MonoBehaviour {
 
 	public void OnClick () {
-		SceneManager.LoadScene(PlayerPrefs.GetString("reScene",""));
+		SceneManager.LoadScene("Title");
 	}
 }
