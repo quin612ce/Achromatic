5697aae7790615f62e00367d56a607f8f146a77e
leo_rabbit30 <mio.rabbit971.snow@gmail.com>
2019-03-19 17:15:15 +0900

    合宿後の変更 主に怒さんのステージ

---
+++ Assets/script_map/RandomTrap3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ときどき、範囲内にランダムにプレハブを作る
+public class RandomTrap3 : MonoBehaviour {
+	public GameObject newPrefab; // 作るプレハブ：Inspectorで指定
+	public float intervalSec = 1; // 作成間隔（秒）：Inspectorで指定する
+
+	void Start() { // 最初に行う
+		// 指定秒ごとに、CreatePrefabをくり返し実行する予約
+		InvokeRepeating("CreatePrefab", intervalSec, intervalSec);
+			}
+
+	void CreatePrefab() {
+		// このオブジェクトの範囲内にランダムに
+		Vector3 newPos = this.transform.position;
+		newPos.x += 1;
+		newPos.y += 1;
+		newPos.z = -5; // 手前に表示
+		// プレハブを作る
+		GameObject newGameObject = Instantiate(newPrefab) as GameObject;
+		newGameObject.transform.position = newPos;
+
+	}
+}
+
