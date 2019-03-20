a36d34d0d114fecdc0a8f92b43e40abd1eb17dd9
leo_rabbit30 <mio.rabbit971.snow@gmail.com>
2019-03-20 18:47:27 +0900

    Map1のテキスト、Map2の岩石、パズルの実装

---
+++ Assets/script_map/FadeScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour {
+	public int count = 0;
+    float alfa;
+    float speed = 0.01f;
+    float red, green, blue;
+
+    void Start () {
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+    }
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			count += 1;
+		}
+		if (count == 36) 
+        {
+			GetComponent<Image>().color = new Color(red, green, blue, alfa);
+			alfa += speed;
+			if (alfa == 225) {
+				//ここに喜さんの移動と暗転の解除を記入予定
+			}
+        }
+    }
+}
--- Assets/script_map/RandomTrap4.cs
+++ Assets/script_map/RandomTrap4.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // 範囲内に1秒ごとにプレハブを作る
-public class RandomTrap3 : MonoBehaviour {
+public class RandomTrap4 : MonoBehaviour {
 	public GameObject newPrefab; // 作るプレハブ：Inspectorで指定
 	public float reposX2,reposY2;
 	public float intervalSec = 0.5f; // 作成間隔（秒）：Inspectorで指定する
@@ -11,20 +11,20 @@ public class RandomTrap3 : MonoBehaviour {
 	void Start() { // 最初に行う
 		// 指定秒ごとに、CreatePrefabをくり返し実行する予約
 		InvokeRepeating("CreatePrefab", intervalSec, intervalSec);
-		reposX2 = -6;
+		reposX2 = -20;
 		reposY2 = 0;
 			}
 
 	void CreatePrefab() {
 		// このオブジェクトの範囲内に
 		Vector3 newPos = this.transform.position;
-		newPos.x = reposX2 - 2.5f;
+		newPos.x = reposX2 + 2.5f;
 		//newPos.y = reposY2 + 2;
 		newPos.z = -5; // 手前に表示
 		reposX2 = newPos.x;
 		reposY2 = newPos.y;
 		// プレハブを作る
-		if (reposX2 >= -18) {
+		if (reposX2 <= -4) {
 		GameObject newGameObject = Instantiate(newPrefab) as GameObject;
 		newGameObject.transform.position = newPos;
 		}
