1a2f79edd075ede5632dbca956592fe3a4f4085b
leo_rabbit30 <mio.rabbit971.snow@gmail.com>
2019-03-23 12:48:40 +0900

    フェードイン、endの背景仮実装

---
+++ Assets/script_map/SceneChange_2toend.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChange_2toend : MonoBehaviour {
+
+	public int count = 0;
+	public GameObject Panel;
+
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			count += 1;
+		}
+		if (count == 26) 
+        {
+            //Panel.SetActive(false);
+			Debug.Log("ToEnd");
+            SceneManager.LoadScene("end");
+        }
+    }
+}
